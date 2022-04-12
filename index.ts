52bf8f5ff51d7d4317cadfaa63dc927a559fe693
Baptiste LYET <baptiste.lyet@agixis.com>
2022-04-12 12:01:53 +0200

    Le code coverage ne fait pas tout

--- index.ts
+++ index.ts
@@ -1,15 +1,7 @@
-export class FizzBuzz{
-    static FizzBuzz(a: number) : string {
-        const a_string = a.toString();
-        let return_value = '';
-
-        if (a % 3 == 0 || a_string.includes('3')) {
-            return_value += 'Fizz';
-        }
-        if (a % 5 == 0 || a_string.includes('5')) {
-            return_value += 'Buzz';
-        }
-
-        return return_value == '' ? a_string : return_value;
+export class Rules{
+    static isLongerThanFiveChar(str: string): boolean{
+        if(str.length > 5)
+            return true;
+        return false;
     }
 }
--- main.test.ts
+++ main.test.ts
@@ -1,3 +1,13 @@
-it("", function () {
-    expect(true).toBe(true);
+import {Rules} from "./index";
+
+it("Should cover the code", function () {
+    Rules.isLongerThanFiveChar("");
+    Rules.isLongerThanFiveChar("dsfsdfd");
+});
+
+it("Should cover the code and test the code", function () {
+    const result1 = Rules.isLongerThanFiveChar("12345");
+    expect(result1).toBe(true);
+    const result2 = Rules.isLongerThanFiveChar("");
+    expect(result2).toBe(false);
 });
